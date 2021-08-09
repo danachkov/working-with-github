53c622bff260f71437d410fbb83d493fc2a2e26d
danachkov <dragomiranachkov@gmail.com>
2021-08-09 13:35:04 +0000

    Second Commit

--- working-with-github/intro.js
+++ working-with-github/intro.js
@@ -1,5 +1,5 @@
 var response = require("http/v4/response");
 
-response.println("Hello World!");
+response.println("Hello there! Today I'm going to show you how to work with the Git view in the Eclipse Dirigible IDE.");
 response.flush();
 response.close();
